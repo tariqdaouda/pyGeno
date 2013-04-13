0ec174f9f82a3aeff73e39fef9e616e507f04f6e
Tariq Daouda <tariq.daouda@umontreal.ca>
2013-04-12 22:27:30 -0400

    Simplified a lot the installation of packages Must move gene_sets into genome folder and add a wey to define the reference genome

--- configuration.py
+++ configuration.py
@@ -1,6 +1,6 @@
 import sys, os, time
 
-DATA_PATH = os.path.dirname(__file__)+ "/pyGenoData/"
+DATA_PATH = os.path.dirname(__file__)+ "/pyGenoData"
 
 pyGeno_BRANCH = "Medusa"
 pyGeno_VERSION_NAME = 'Skiny Cobra'
@@ -11,9 +11,18 @@ pyGeno_VERSION_BUILD_TIME = time.ctime(os.path.getmtime(__file__))
 pyGeno_VERSION_TUPLE = (pyGeno_BRANCH, pyGeno_VERSION_NAME, pyGeno_VERSION_RELEASE_LEVEL, pyGeno_VERSION_NUMBER, pyGeno_VERSION_BUILD_TIME )
 pyGeno_VERSION_STR = "Branch: %s, Name: %s, Release Level: %s, Version: %s, Build time: %s" % pyGeno_VERSION_TUPLE
 
+def currentVersion():
+	"""returns a tuple describing pyGeno's current version"""
+	return pyGeno_VERSION_TUPLE
+
+def currentVersion_str():
+	"""returns pyGeno's current version in a human redable way"""
+	return conf.pyGeno_VERSION_STR
+	
 def configure():
 	if sys.version_info[0] < 2 or (sys.version_info[0] > 2  and sys.version_info[1] < 7) :
 		print "==> FATAL : pyGeno only works with python 2.7 and above, please upgrade your version"
 		sys.exit(1)
 
 configure()
+
