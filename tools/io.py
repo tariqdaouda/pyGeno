45b520fc7171729adb05279e99ea8062e7bab613
Tariq Daouda <tariq.daouda@umontreal.ca>
2013-05-16 18:22:09 -0400

    dded io

---
+++ tools/io.py
@@ -0,0 +1,25 @@
+def enterConfirm_prompt(enterMsg) :
+	stopi = False
+	while not stopi :
+		print "====\n At any time you can quit by entering 'quit'\n===="
+		vali = raw_input(enterMsg)
+		if vali.lower() == 'quit' :
+			vali = None
+			stopi = True
+		else :
+			print "You've entered:\n\t%s" % vali
+			valj = confirm_prompt("")
+			if valj == 'yes' :
+				stopi = True
+			if valj == 'quit' :
+				vali = None
+				stopi = True
+				
+	return vali
+
+def confirm_prompt(preMsg) :
+	while True :
+		val = raw_input('%splease confirm ("yes", "no", "quit"): ' % preMsg)
+		if val.lower() == 'yes' or val.lower() == 'no' or val.lower() == 'quit':
+			return val.lower()
+		
