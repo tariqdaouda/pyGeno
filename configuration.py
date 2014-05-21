9ef721c300c0cde5cf0c7a9fae8b27b7813e365f
Tariq Daouda <tariq.daouda@umontreal.ca>
2014-05-21 19:20:25 -0400

    Now seetings path is in ~/.pyGeno/config.ini if the fle soed not ecisst a default one is created

--- configuration.py
+++ configuration.py
@@ -1,4 +1,6 @@
 import sys, os, time
+from ConfigParser import SafeConfigParser
+
 import rabaDB.setup
 
 import user_config as uc
@@ -15,8 +17,11 @@ pyGeno_VERSION_NUMBER = 14.02
 pyGeno_VERSION_BUILD_TIME = time.ctime(os.path.getmtime(__file__))
 
 pyGeno_RABA_NAMESPACE = 'pyGenoRaba'
-pyGeno_RABA_DBFILE = os.path.normpath('%s/pyGenoRaba.db' % uc.pyGeno_SETTINGS_PATH)
-pyGeno_DATA_PATH = os.path.normpath('%s/data' % uc.pyGeno_SETTINGS_PATH)
+
+pyGeno_SETTINGS_DIR = os.path.normpath(os.path.expanduser('~/.pyGeno/'))
+pyGeno_SETTINGS_PATH = None
+pyGeno_RABA_DBFILE = None
+pyGeno_DATA_PATH = None
 
 db = None
 
@@ -37,16 +42,43 @@ def checkPythonVersion() :
 def getGenomeSequencePath(specie, name) :
 	return os.path.normpath(pyGeno_DATA_PATH+'/%s/%s' % (specie.lower(), name))
 
+def createDefaultConfigFile() :
+	s = "[pyGeno_config]\nsettings_dir = %s" % pyGeno_SETTINGS_DIR
+	f = open('%s/config.ini' % pyGeno_SETTINGS_DIR, 'w')
+	f.write(s)
+	f.close()
+
+def getSettingsPath() :
+	
+	parser = SafeConfigParser()
+	try :
+		parser.read(os.path.normpath(pyGeno_SETTINGS_DIR+'/config.ini'))
+		return parser.get('pyGeno_config', 'settings_dir')
+	except :
+		createDefaultConfigFile()
+		return getSettingsPath()
+
 def pyGeno_init() :
 	"This function is automaticly called at launch"
 	
 	global db
 	
+	global pyGeno_SETTINGS_PATH
+	global pyGeno_RABA_DBFILE
+	global pyGeno_DATA_PATH
+	
 	if not checkPythonVersion() :
 		raise PythonVersionError("==> FATAL: pyGeno only works with python 2.7 and above, please upgrade your python version")
 
-	if not os.path.exists(uc.pyGeno_SETTINGS_PATH) :
-		os.makedirs(uc.pyGeno_SETTINGS_PATH)
+	if not os.path.exists(pyGeno_SETTINGS_DIR) :
+		os.makedirs(pyGeno_SETTINGS_DIR)
+	
+	pyGeno_SETTINGS_PATH = getSettingsPath()
+	pyGeno_RABA_DBFILE = os.path.normpath('%s/pyGenoRaba.db' % pyGeno_SETTINGS_PATH)
+	pyGeno_DATA_PATH = os.path.normpath('%s/data' % pyGeno_SETTINGS_PATH)
+	
+	if not os.path.exists(pyGeno_SETTINGS_PATH) :
+		os.makedirs(pyGeno_SETTINGS_PATH)
 
 	if not os.path.exists(pyGeno_DATA_PATH) :
 		os.makedirs(pyGeno_DATA_PATH)
--- user_config.py
+++
@@ -1,4 +0,0 @@
-import os
-
-#this file contains the user's specifc configuration
-pyGeno_SETTINGS_PATH = os.path.expanduser('~/.pyGeno/')
