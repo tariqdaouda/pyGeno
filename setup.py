b287f923c7e4e825016bcb8b4d3fe94d589a86cd
tariqdaouda <tariq.daouda@umontreal.ca>
2014-11-04 14:38:06 -0500

    maj

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setup(
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
     package_data={
-        'bootstrap': ['*.txt', '*.rst', '*.tar.gz'],
+        '': ['*.txt', '*.rst', '*.tar.gz'],
     },
 
     # Although 'package_data' is the preferred approach, in some case you may
