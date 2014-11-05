e78cd4cd5527af0eafce6247ffbd40979f12945d
tariqdaouda <tariq.daouda@umontreal.ca>
2014-11-05 13:18:32 -0500

    Bootstrap human ref y only

--- pyGeno/bootstrap.py
+++ pyGeno/bootstrap.py
@@ -7,6 +7,10 @@ def importHumanReference() :
 	path = os.path.join(this_dir, "bootstrap_data", "GRCh37.75.tar.gz")
 	importGenome(path)
 
+def importHumanReference_YOnly() :
+	path = os.path.join(this_dir, "bootstrap_data", "GRCh37.75_Y-Only.tar.gz")
+	importGenome(path)
+
 def importDummySRY() :
 	path = os.path.join(this_dir, "bootstrap_data", "dummySRY.tar.gz")
 	importSNPs(path)
