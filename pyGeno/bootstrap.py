1cea2b31b76d9704c9a4170db80162d4ec0e1e4c
tariqdaouda <tariq.daouda@umontreal.ca>
2014-11-11 15:38:39 -0500

    added function to list datawraps : todo, print description

--- pyGeno/bootstrap.py
+++ pyGeno/bootstrap.py
@@ -4,6 +4,10 @@ import os
 
 this_dir, this_filename = os.path.split(__file__)
 
+def listDataWraps() :
+	"""Lists all the datawraps pyGeno comes with"""
+	print os.listdir(this_dir)
+
 def importHumanReferenceYOnly() :
 	"Importing only the Y chromosome of the Human Reference Genome. Useful for playing a bit with pyGeno."
 	path = os.path.join(this_dir, "bootstrap_data", "GRCh37.75_Y-Only.tar.gz")
@@ -15,6 +19,7 @@ def importDummySRY() :
 	importSNPs(path)
 
 def importHumanReference() :
-	"Importes the Human Reference Genome. This may take a while, but it's done only once."
-	path = os.path.join(this_dir, "bootstrap_data", "GRCh36.75.tar.gz")
+	""""Importes the Human Reference Genome. This may take a while, depending on the computers and 
+	indexes in the database. But it's done only once."""
+	path = os.path.join(this_dir, "bootstrap_data", "GRCh37.75.tar.gz")
 	importGenome(path)
