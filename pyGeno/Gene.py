2a020466f4ef1e465890638f3eb51349c3f0b137
Adel Qalieh <aqalieh95@gmail.com>
2017-06-11 18:33:55 -0400

    Misc type check fixes in Gene

--- pyGeno/Gene.py
+++ pyGeno/Gene.py
@@ -36,24 +36,22 @@ class Gene(pyGenoRabaObjectWrapper):
 
     def _makeLoadQuery(self, objectType, *args, **coolArgs):
         if issubclass(objectType, SNP_INDEL):
-            f = RabaQuery(
-                objectType, namespace=self._wrapped_class._raba_namespace)
+            f = RabaQuery(objectType, namespace=self._wrapped_class._raba_namespace)
             coolArgs['species'] = self.genome.species
             coolArgs['chromosomeNumber'] = self.chromosome.number
             coolArgs['start >='] = self.start
             coolArgs['start <'] = self.end
 
-            if len(args) > 0 and type(args[0]) is types.ListType:
+            if len(args) > 0 and isinstance(args[0], list):
                 for a in args[0]:
-                    if type(a) is types.DictType:
+                    if isinstance(a, dict):
                         f.addFilter(**a)
             else:
                 f.addFilter(*args, **coolArgs)
 
             return f
 
-        return pyGenoRabaObjectWrapper._makeLoadQuery(self, objectType, *args,
-                                                      **coolArgs)
+        return pyGenoRabaObjectWrapper._makeLoadQuery(self, objectType, *args, **coolArgs)
 
     def __str__(self):
         return "Gene, name: %s, id: %s, strand: '%s' > %s" % (
