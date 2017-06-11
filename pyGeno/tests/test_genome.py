0f8b8ecb3ac6309b6384773037e14b76138de9e9
Adel Qalieh <aqalieh95@gmail.com>
2017-06-11 18:52:53 -0400

    YAPF all tests and fix style

--- pyGeno/tests/test_genome.py
+++ pyGeno/tests/test_genome.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import unittest
 from pyGeno.Genome import *
 
@@ -5,144 +6,129 @@ import pyGeno.bootstrap as B
 
 
 class pyGenoSNPTests(unittest.TestCase):
+    def setUp(self):
+        self.ref = Genome(name='GRCh37.75_Y-Only')
 
-	def setUp(self):
-		self.ref = Genome(name = 'GRCh37.75_Y-Only')
+    def tearDown(self):
+        pass
 
-	def tearDown(self):
-		pass
+    def test_vanilla(self):
+        dummy = Genome(name='GRCh37.75_Y-Only', SNPs='dummySRY_AGN')
+        persProt = dummy.get(Protein, id='ENSP00000438917')[0]
+        refProt = self.ref.get(Protein, id='ENSP00000438917')[0]
 
-	#@unittest.skip("skipping")
-	def test_vanilla(self) :
-		dummy = Genome(name = 'GRCh37.75_Y-Only', SNPs = 'dummySRY_AGN')
-		persProt = dummy.get(Protein, id = 'ENSP00000438917')[0]
-		refProt = self.ref.get(Protein, id = 'ENSP00000438917')[0]
+        self.assertEqual('ATGCAATCATATGCTTCTGC', refProt.transcript.cDNA[:20])
+        self.assertEqual('HTGCAATCATATGCTTCTGC', persProt.transcript.cDNA[:20])
 
-		self.assertEqual('ATGCAATCATATGCTTCTGC', refProt.transcript.cDNA[:20])
-		self.assertEqual('HTGCAATCATATGCTTCTGC', persProt.transcript.cDNA[:20])
+    def test_noModif(self):
+        from pyGeno.SNPFiltering import SNPFilter
 
-	#@unittest.skip("skipping")
-	def test_noModif(self) :
-		from pyGeno.SNPFiltering import SNPFilter
+        class MyFilter(SNPFilter):
+            def __init__(self):
+                SNPFilter.__init__(self)
 
-		class MyFilter(SNPFilter) :
-			def __init__(self) :
-				SNPFilter.__init__(self)
+            def filter(self, chromosome, dummySRY_AGN):
+                return None
 
-			def filter(self, chromosome, dummySRY_AGN) :
-				return None
+        dummy = Genome(name='GRCh37.75_Y-Only', SNPs='dummySRY_AGN', SNPFilter=MyFilter())
+        persProt = dummy.get(Protein, id='ENSP00000438917')[0]
+        refProt = self.ref.get(Protein, id='ENSP00000438917')[0]
 
-		dummy = Genome(name = 'GRCh37.75_Y-Only', SNPs = 'dummySRY_AGN', SNPFilter = MyFilter())
-		persProt = dummy.get(Protein, id = 'ENSP00000438917')[0]
-		refProt = self.ref.get(Protein, id = 'ENSP00000438917')[0]
+        self.assertEqual(persProt.transcript.cDNA[:20],
+                         refProt.transcript.cDNA[:20])
 
-		self.assertEqual(persProt.transcript.cDNA[:20], refProt.transcript.cDNA[:20])
+    def test_insert(self):
+        from pyGeno.SNPFiltering import SNPFilter
 
-	def test_insert(self) :
-		from pyGeno.SNPFiltering import SNPFilter
+        class MyFilter(SNPFilter):
+            def __init__(self):
+                SNPFilter.__init__(self)
 
-		class MyFilter(SNPFilter) :
-			def __init__(self) :
-				SNPFilter.__init__(self)
+            def filter(self, chromosome, dummySRY_AGN):
+                from pyGeno.SNPFiltering import SequenceInsert
 
-			def filter(self, chromosome, dummySRY_AGN) :
-				from pyGeno.SNPFiltering import  SequenceInsert
+                refAllele = chromosome.refSequence[dummySRY_AGN.start]
+                return SequenceInsert('TCA')
 
-				refAllele = chromosome.refSequence[dummySRY_AGN.start]
-				return SequenceInsert('TCA')
+        dummy = Genome(name='GRCh37.75_Y-Only', SNPs='dummySRY_AGN', SNPFilter=MyFilter())
+        persProt = dummy.get(Protein, id='ENSP00000438917')[0]
+        refProt = self.ref.get(Protein, id='ENSP00000438917')[0]
 
-		dummy = Genome(name = 'GRCh37.75_Y-Only', SNPs = 'dummySRY_AGN', SNPFilter = MyFilter())
-		persProt = dummy.get(Protein, id = 'ENSP00000438917')[0]
-		refProt = self.ref.get(Protein, id = 'ENSP00000438917')[0]
+        print(refProt.transcript.data[:20])
+        print(persProt.transcript.data[:20])
 
-		print refProt.transcript.data[:20]
-		print persProt.transcript.data[:20]
+        self.assertEqual('ATGCAATCATATGCTTCTGC', refProt.transcript.cDNA[:20])
+        self.assertEqual('TGAATGCAATCATATGCTTC', persProt.transcript.cDNA[:20])
 
-		self.assertEqual('ATGCAATCATATGCTTCTGC', refProt.transcript.cDNA[:20])
-		self.assertEqual('TGAATGCAATCATATGCTTC', persProt.transcript.cDNA[:20])
+    def test_SNP(self):
+        from pyGeno.SNPFiltering import SNPFilter
 
-	#@unittest.skip("skipping")
-	def test_SNP(self) :
-		from pyGeno.SNPFiltering import SNPFilter
+        class MyFilter(SNPFilter):
+            def __init__(self):
+                SNPFilter.__init__(self)
 
-		class MyFilter(SNPFilter) :
-			def __init__(self) :
-				SNPFilter.__init__(self)
+            def filter(self, chromosome, dummySRY_AGN):
+                from pyGeno.SNPFiltering import SequenceSNP
 
-			def filter(self, chromosome, dummySRY_AGN) :
-				from pyGeno.SNPFiltering import SequenceSNP
+                return SequenceSNP(dummySRY_AGN.alt)
 
-				return SequenceSNP(dummySRY_AGN.alt)
+        dummy = Genome(name='GRCh37.75_Y-Only', SNPs='dummySRY_AGN', SNPFilter=MyFilter())
+        # persProt = dummy.get(Protein, id = 'ENSP00000438917')[0]
+        persProt = dummy.get(Protein, id='ENSP00000438917')[0]
+        # print persProt, dummy.count(Protein)
+        # persProt = persProt[0]
+        refProt = self.ref.get(Protein, id='ENSP00000438917')[0]
+        self.assertEqual('M', refProt.sequence[0])
+        self.assertEqual('L', persProt.sequence[0])
 
-		dummy = Genome(name = 'GRCh37.75_Y-Only', SNPs = 'dummySRY_AGN', SNPFilter = MyFilter())
-		# persProt = dummy.get(Protein, id = 'ENSP00000438917')[0]
-		persProt = dummy.get(Protein, id = 'ENSP00000438917')[0]
-		# print persProt, dummy.count(Protein)
-		# persProt = persProt[0]
-		refProt = self.ref.get(Protein, id = 'ENSP00000438917')[0]
-		self.assertEqual('M', refProt.sequence[0])
-		self.assertEqual('L', persProt.sequence[0])
+    def test_deletion(self):
+        from pyGeno.SNPFiltering import SNPFilter
 
-	#@unittest.skip("skipping")
-	def test_deletion(self) :
-		from pyGeno.SNPFiltering import SNPFilter
+        class MyFilter(SNPFilter):
+            def __init__(self):
+                SNPFilter.__init__(self)
 
-		class MyFilter(SNPFilter) :
-			def __init__(self) :
-				SNPFilter.__init__(self)
+            def filter(self, chromosome, dummySRY_AGN):
+                from pyGeno.SNPFiltering import SequenceDel
 
-			def filter(self, chromosome, dummySRY_AGN) :
-				from pyGeno.SNPFiltering import SequenceDel
+                refAllele = chromosome.refSequence[dummySRY_AGN.start]
+                return SequenceDel(1)
 
-				refAllele = chromosome.refSequence[dummySRY_AGN.start]
-				return SequenceDel(1)
+        dummy = Genome(name='GRCh37.75_Y-Only', SNPs='dummySRY_AGN', SNPFilter=MyFilter())
+        persProt = dummy.get(Protein, id='ENSP00000438917')[0]
+        refProt = self.ref.get(Protein, id='ENSP00000438917')[0]
 
-		dummy = Genome(name = 'GRCh37.75_Y-Only', SNPs = 'dummySRY_AGN', SNPFilter = MyFilter())
-		persProt = dummy.get(Protein, id = 'ENSP00000438917')[0]
-		refProt = self.ref.get(Protein, id = 'ENSP00000438917')[0]
+        self.assertEqual('ATGCAATCATATGCTTCTGC', refProt.transcript.cDNA[:20])
+        self.assertEqual('TGCAATCATATGCTTCTGCT', persProt.transcript.cDNA[:20])
 
-		self.assertEqual('ATGCAATCATATGCTTCTGC', refProt.transcript.cDNA[:20])
-		self.assertEqual('TGCAATCATATGCTTCTGCT', persProt.transcript.cDNA[:20])
+    def test_bags(self):
+        dummy = Genome(name='GRCh37.75_Y-Only')
+        self.assertEqual(dummy.wrapped_object, self.ref.wrapped_object)
 
-	#@unittest.skip("skipping")
-	def test_bags(self) :
-		dummy = Genome(name = 'GRCh37.75_Y-Only')
-		self.assertEqual(dummy.wrapped_object, self.ref.wrapped_object)
+    def test_prot_find(self):
+        prot = self.ref.get(Protein, id='ENSP00000438917')[0]
+        needle = prot.sequence[:10]
+        self.assertEqual(0, prot.find(needle))
+        needle = prot.sequence[-10:]
+        self.assertEqual(len(prot) - 10, prot.find(needle))
 
-	#@unittest.skip("skipping")
-	def test_prot_find(self) :
-		prot = self.ref.get(Protein, id = 'ENSP00000438917')[0]
-		needle = prot.sequence[:10]
-		self.assertEqual(0, prot.find(needle))
-		needle = prot.sequence[-10:]
-		self.assertEqual(len(prot)-10, prot.find(needle))
+    def test_trans_find(self):
+        trans = self.ref.get(Transcript, name="SRY-001")[0]
+        self.assertEqual(0, trans.find(trans[:5]))
 
-	#@unittest.skip("skipping")
-	def test_trans_find(self) :
-		trans = self.ref.get(Transcript, name = "SRY-001")[0]
-		self.assertEqual(0, trans.find(trans[:5]))
 
-	# @unittest.skip("remote server down")
-	# def test_import_remote_genome(self) :
-		# self.assertRaises(KeyError, B.importRemoteGenome, "Human.GRCh37.75_Y-Only.tar.gz")
+def runTests():
+    try:
+        B.importGenome("Human.GRCh37.75_Y-Only.tar.gz")
+    except KeyError:
+        print("--> Seems to already exist in db")
 
-	# @unittest.skip("remote server down")
-	# def test_import_remote_snps(self) :
-		# self.assertRaises(KeyError, B.importRemoteSNPs, "Human_agnostic.dummySRY.tar.gz")
+    try:
+        B.importSNPs("Human_agnostic.dummySRY.tar.gz")
+    except KeyError:
+        print("--> Seems to already exist in db")
+    unittest.main()
 
-def runTests() :
-	try :
-		B.importGenome("Human.GRCh37.75_Y-Only.tar.gz")
-	except KeyError :
-		print "--> Seems to already exist in db"
 
-	try :
-		B.importSNPs("Human_agnostic.dummySRY.tar.gz")
-	except KeyError :
-		print "--> Seems to already exist in db"
-	# import time
-	# time.sleep(10)
-	unittest.main()
-
-if __name__ == "__main__" :
-	runTests()
+if __name__ == "__main__":
+    runTests()
