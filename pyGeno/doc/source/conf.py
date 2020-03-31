a552969924b7b7a00e7fed6d890a408c9587799e
joerg84 <joerg.schad@gmail.com>
2020-03-31 19:39:38 +0200

    Updated Copyright.

--- pyGeno/doc/source/conf.py
+++ pyGeno/doc/source/conf.py
@@ -55,7 +55,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'pyGeno'
-copyright = '2014, Tariq Daouda'
+copyright = '2020, Tariq Daouda'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
