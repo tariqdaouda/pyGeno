77fd4dd4313461b8d3ebd81d6031bd7a563129bc
Albert Feghaly <albert.feghaly@umontreal.ca>
2021-09-15 23:19:52 -0400

    Bump version to 2.1.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
 setup(
     name='pyGeno',
 
-    version='2.0.1',
+    version='2.1.0',
 
     description='A python package for Personalized Genomics and Proteomics',
     long_description=long_description,
